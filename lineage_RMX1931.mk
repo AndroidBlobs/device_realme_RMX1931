699c0a84ca045ef16cb0f097d654532391e2f1b4
ShivamKumarJha <jha.shivam3@gmail.com>
2020-02-10 20:14:34 +0000

    msmnile-user-9-PKQ1.190626.001-eng.root.20200116.220505-release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- lineage_RMX1931.mk
+++ lineage_RMX1931.mk
@@ -17,7 +17,7 @@ PRODUCT_MODEL := realme X2 Pro
 PRODUCT_GMS_CLIENTID_BASE := android-realme
 TARGET_VENDOR := realme
 TARGET_VENDOR_PRODUCT_NAME := RMX1931
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="msmnile-user 9 PKQ1.190626.001 eng.root.20191225.111704 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="msmnile-user 9 PKQ1.190626.001 eng.root.20200116.220505 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := msmnile-user-9-PKQ1.190626.001-eng.root.20191225.111704-release-keys
+BUILD_FINGERPRINT := msmnile-user-9-PKQ1.190626.001-eng.root.20200116.220505-release-keys
